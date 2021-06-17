97b159964a41c85365e6ae453b5712fde6550798
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-06-17 15:09:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+var count = 1;
+var main = function(req, res)
+{
+    res.write(String(i));
+    res.end();
+    count += 1;
+}
+
+http.createServer(main).listen(4200);
