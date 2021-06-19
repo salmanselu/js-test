7975a2af2926e9901e41470d55abc9fa55446347
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-19 18:41:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ var count = 0;
 var main = function(req, res)
 {
     fs.readFile('index.html', function(err, data){
-        res.write("<h1>Number of times this page was viewed:" + req.url+"</h1>" + data);
+        res.write("<h1>Number of times this page was viewed:" + q+"</h1>" + data);
         res.end();
     });
-    
+    var q = url.parse(req.url, true).query;
     count += 0.5;
 }
 
