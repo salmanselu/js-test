8e650b27de5ba1a6d240b3d5a617f47f55c0d35f
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-06-19 23:55:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,25 +2,25 @@ var http = require("http");
 var fs = require('fs');
 var url = require('url');
 
-var count = 0;
 var main = function(req, res)
 {
     fs.readFile('login.html', function(err, data){
 
-        if(q.name === 'Nafeela' || q.name === 'Salman')
-        {
-            var body = "<h1>Hii " + q.name +"</h1>"
-        }
-        else
+        if(q.name === '/')
         {
             var body = data
         }
+        else
+        {
+            var body = "<h1>Hii " + q.name +"</h1>"
+        }
+       
+        
         res.write(body);
         res.end();
         
     });
     var q = url.parse(req.url, true).query;
-    count += 0.5;
 }
 
 http.createServer(main).listen(process.env.PORT);
