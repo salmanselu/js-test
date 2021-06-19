71834437cde0dc93a282475750362ac4b3792891
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-19 19:14:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ var count = 0;
 var main = function(req, res)
 {
     fs.readFile('index.html', function(err, data){
-        res.write("<h1>Number of times this page was viewed:" + q.name+"</h1>" + data);
+
+        if(q.name === 'Nafeela' || q.name === 'Salman'){
+            var body = "Hii" + q.name+"</h1>" + data
+        }
+        else
+        {
+            var body = "<h1>Enter username<h1>"
+        }
+        res.write(body);
         res.end();
+    
     });
     var q = url.parse(req.url, true).query;
     count += 0.5;
