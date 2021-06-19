c13c191ff1188d3c8e339e8237d34115671ad692
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-19 18:39:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var http = require("http");
 var fs = require('fs');
+var url = require('url');
+
 var count = 0;
 var main = function(req, res)
 {
     fs.readFile('index.html', function(err, data){
-        res.write("<h1>Number of times this page was viewed:" + String(count)+"</h1>" + data);
+        res.write("<h1>Number of times this page was viewed:" + req.url+"</h1>" + data);
         res.end();
     });
+    
     count += 0.5;
 }
 
