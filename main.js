2a659bb61ab4bee6bb93f74f82a01c5134c1a914
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-19 18:05:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var http = require("http");
 var count = 0;
 var main = function(req, res)
 {
-    res.write("Number of times this page was viewed:" + String(count));
+    res.write("<h1>Number of times this page was viewed:" + String(count)+"</h1>");
     res.end();
     count += 0.5;
 }
