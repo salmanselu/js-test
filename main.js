d6fa73475109eb696a5088ce5b53f202f9035cad
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-19 18:49:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ var count = 0;
 var main = function(req, res)
 {
     fs.readFile('index.html', function(err, data){
-        res.write("<h1>Number of times this page was viewed:" + q+"</h1>" + data);
+        res.write("<h1>Number of times this page was viewed:" + q.name+"</h1>" + data);
         res.end();
     });
     var q = url.parse(req.url, true).query;
     count += 0.5;
 }
 
-http.createServer(main).listen(process.env.PORT);
+http.createServer(main).listen(process.env.PORT | 4200);
