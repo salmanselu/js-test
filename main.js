e6236f98562c851440f02fa0a2df2adf9c557ba7
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-22 16:02:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,35 +2,20 @@ var http = require("http");
 var fs = require('fs');
 var url = require('url');
 
-global.task = ' ';
-global.body = ' ';
+
 var main = function(req, res)
 {
-    fs.readFile('login.html', function(err, data){
-        
-        var q = url.parse(req.url, true).query;
-        body = task + data;
-        if(req.url === '/')
-        {
-            
-        }
-        else if(q.clear === 'clear')
-        {
-            task = ' ';
-            body = ' ';
-            body += data;
-        }
-        else if(q.task)
-        { 
-            var date = Date();
-            
-            task += '<p class="font-face: sans-serif;">'+ q.task +'<br><a style="color: gray;">'+ String(date) +'</a></p><br><hr>';
-            body = task + data;
-        }       
-        res.write(body);
-        //res.end();
+    var q = url.parse(req.url, true);
+    var filename = "."+ q.pathname;
+    fs.readFile(filename, function(err, data){
+    if(err){
+        res.writeHead(404, {'Content-Type':'text/html'})
+        return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write(data);
+    return res.end();
     });
-    
 }
 
 http.createServer(main).listen(process.env.PORT || 4200);
