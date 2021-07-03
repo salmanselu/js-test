77b4ca4051248e9ce35b4e96c7c242be85dbc1e3
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-07-04 00:45:33 +0530

    Hshshs

--- main.js
+++ main.js
@@ -5,17 +5,14 @@ var url = require('url');
 
 var main = function(req, res)
 {
-    var q = url.parse(req.url, true);
-    var filename = "."+ q.pathname;
-    fs.readFile(filename, function(err, data){
-    if(err){
-        res.writeHead(404, {'Content-Type':'text/html'})
-        return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.write(data);
-    return res.end();
-    });
+var r;
+var g;
+var b;
+for(r=0;r<256;r++)
+for(g=0;g<256;g++)
+for(b=0;b<256;b++)
+res.write('<div style="background-color:rgb('+r+','+g+','+b+',' +'"> </div>');
+
 }
 
 http.createServer(main).listen(process.env.PORT || 4200);
