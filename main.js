f0711a431af706813f1dcad3b3b6503ae3653ce3
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-07-04 00:52:14 +0530

    Gshsh

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 var http = require("http");
-var fs = require('fs');
-var url = require('url');
-
 
 var main = function(req, res)
 {
@@ -12,7 +9,6 @@ for(r=0;r<256;r++)
 for(g=0;g<256;g++)
 for(b=0;b<256;b++)
 res.write('<div style="background-color:rgb('+r+','+g+','+b+',' +'"> </div>');
-
 }
 
 http.createServer(main).listen(process.env.PORT || 4200);
