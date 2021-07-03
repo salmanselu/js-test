62b4a4558edde15ff9ba0cb3657b5d52a71f6c88
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-07-04 00:55:07 +0530

    Cvg

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var b;
 for(r=0;r<256;r++)
 for(g=0;g<256;g++)
 for(b=0;b<256;b++)
-res.write('<div style="background-color:rgb('+r+','+g+','+b+',' +'"> </div>');
+res.write(`<div style="background-color:rgb(${r},${g},${b})"> </div>`);
 }
 
 http.createServer(main).listen(process.env.PORT || 4200);
